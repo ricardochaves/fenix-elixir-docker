a278ad41be6175062df252e5504078737c7ab121
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2018-08-07 21:42:47 -0300

    get external api

--- lib/oraculo_web/controllers/api_controller.ex
+++ lib/oraculo_web/controllers/api_controller.ex
@@ -3,9 +3,28 @@ defmodule OraculoWeb.ApiController do
   
     def index(conn, _params) do
 
+      a = GitHub.get_user_info()
 
-        
-      json conn, %{"name": "ricardo"}
+      json conn, %{"name": "ricardo", "body": a}
     end
   end
+
+  defmodule GitHub do
+    use HTTPotion.Base
   
+    def get_user_info() do
+      GitHub.get("users/myfreeweb").body["public_repos"]
+    end
+
+    def process_url(url) do
+      "https://api.github.com/" <> url
+    end
+  
+    def process_request_headers(headers) do
+      Dict.put headers, :"User-Agent", "test-agent"
+    end
+  
+    def process_response_body(body) do
+      body |> Poison.decode!
+    end
+  end
